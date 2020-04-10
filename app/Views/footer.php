d8c6e10546d0a0e437cf41e8175518629242e166
Weedged <jvlian.mueller.business@gmail.com>
2020-04-10 16:18:49 +0200

    include navbar, footer, header

--- app/Views/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="py-4 text-center bg-white">
-    <span class="text-small text-muted">&copy; <?= $date ?>
-        <a href="https://www.first-black-platoon.eu"><?php printf($_SERVER['SERVER_NAME']); ?></a>
-        - Alle Rechte vorbehalten</span>
-</footer>
-<script src="js/meta.js"></script>
-</body>
-</html>
