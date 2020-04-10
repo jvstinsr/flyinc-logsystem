3967f25f8fa0612019d62d104dd0ef9aba11a4c8
Weedged <jvlian.mueller.business@gmail.com>
2020-04-10 19:04:56 +0200

    title

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,9 +1,9 @@
 <div class="container">
-    <h3 class="h3">Mitarbeiter Anwesenheit am {{heutiger Tag}}</h3>
+    <h3 class="h3 mt-sm-4">Mitarbeiter Anwesenheit am {{heutigen Tag}}</h3>
     <div id="user-time" class="container mt-4 mb-2 px-2 py-4 border rounded">
         CONTENT
     </div>
-    <h3 class="h3">Wochenstatistik in Stunden</h3>
+    <h3 class="h3 mt-sm-4">Wochenstatistik</h3>
     <div id="hour-per-week" class="mt-2 mb-4 px-2 py-4 border rounded">
         CONTENT
     </div>
