9450d1bba225a3c7217e0ad641b5f40a9146af3b
webedged <jvlian.mueller.business@gmail.com>
2020-04-10 15:59:43 +0200

    include navbar, footer, header

---
+++ app/Views/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="favicon.png" rel="shortcut icon" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $title ?></title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" media="all" rel="stylesheet" type="text/css">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/ab959ee33f.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
