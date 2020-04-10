9f2463aeceeabb57e9b73bbc590662f55623bcec
Weedged <jvlian.mueller.business@gmail.com>
2020-04-10 18:47:55 +0200

    title

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,5 +1,5 @@
 <footer class="py-4 text-center bg-white">
-    <span class="text-small text-muted">&copy; <?= $date ?>
+    <span class="text-small text-muted">&copy;<?= $date ?>
         <a href="https://www.first-black-platoon.eu"><?php printf($_SERVER['SERVER_NAME']); ?></a>
         - Alle Rechte vorbehalten</span>
 </footer>
